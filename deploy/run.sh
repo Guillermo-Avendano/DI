71f454ad31804cd280dfc4303974a93273b246c9
Guillermo-Avendano <avendano.guillermo@gmail.com>
2023-11-24 09:02:45 -0300

    va

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo mkdir -p conf/ appl/ datbas/ solr_data/ idxupd_conf/ idxupd_logs/ tomcat_logs/ tomcat_conf/
-sudo chmod -R 777 conf/ appl/ datbas/ solr_data/ idxupd_conf/ idxupd_logs/ tomcat_logs/ tomcat_conf/
 docker-compose up -d
 
 
