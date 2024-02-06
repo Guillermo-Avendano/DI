996928104d6b610388f5c7435b317cfe4b519005
Guillermo-Avendano <avendano.guillermo@gmail.com>
2024-02-06 11:59:30 -0300

    va

---
+++ deploy/access.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo chmod -R 777 di_server_datbas/
+sudo chmod -R 777 di_server_appl/
+
+sudo chmod -R 777 di_indexupd_conf/
+sudo chmod -R 777 di_indexupd_logs/
+
+sudo chmod -R 777 di_webapp_conf/
+sudo chmod -R 777 di_webapp_logs/
+
+sudo chmod -R 777 di_scanners_workspace/
--- deploy/run.sh
+++ deploy/run.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
 docker-compose up -d --remove-orphans
-
-sudo chmod -R 775 di_server_datbas/
-sudo chmod -R 775 di_server_appl/
-
-sudo chmod -R 775 di_indexupd_conf/
-sudo chmod -R 775 di_indexupd_logs/
-
-sudo chmod -R 775 di_webapp_conf/
-sudo chmod -R 775 di_webapp_logs/
-
-sudo chmod -R 775 di_scanners_workspace/
-
-
-
