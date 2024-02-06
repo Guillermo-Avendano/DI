6496e2bd7a2c429b41ead7d7d070e2ab60454a56
Guillermo-Avendano <avendano.guillermo@gmail.com>
2024-02-06 10:31:42 -0300

    va

--- deploy/destroy.sh
+++ deploy/destroy.sh
@@ -2,3 +2,12 @@
 
 docker-compose down -v
 
+sudo rm -rf di_server_datbas/
+sudo rm -rf di_server_appl/
+
+sudo rm -rf di_indexupd_conf/
+sudo rm -rf di_indexupd_logs/
+
+sudo rm -rf di_webapp_conf/
+sudo rm -rf di_webapp_logs/
+
--- deploy/run.sh
+++ deploy/run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 docker-compose up -d --remove-orphans
 
+sudo chmod -R +r,+w di_server_datbas/
+sudo chmod -R +r,+wdi_server_appl/
+
+sudo chmod -R +r,+w di_indexupd_conf/
+sudo chmod -R +r,+w di_indexupd_logs/
+
+sudo chmod -R +r,+w di_webapp_conf/
+sudo chmod -R +r,+w di_webapp_logs/
 
