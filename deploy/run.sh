a31e20919825eae911e54930fb2602367ed69996
Guillermo-Avendano <avendano.guillermo@gmail.com>
2024-02-07 13:09:11 -0300

    va

--- deploy/run.sh
+++ deploy/run.sh
@@ -2,6 +2,32 @@
 
 if [ ! -d "./di_server_datbas" ]; then
    docker-compose -f docker-compose.db.yaml up -d
+   sudo chmod -R 777 di_server_datbas/
+   DB_PATH=./di_server_datbas/
+   for i in {1..3}; do
+        md5=$(md5sum $DB_PATH/d1.rodb | awk '{print $1}')
+        expected=$(cat $DB_PATH/d1.md5 | awk '{print $1}')
+        if [ "$md5" == "$expected" ]; then
+            echo "$DB_PATH/d1.rodb md5sum verified, attempt $i"
+            d1=true
+            break
+        else
+            echo "$DB_PATH/d1.rodb md5sum not verified, attempt $i"
+        fi
+   done
+
+   # Verificar d2.rodb
+   for i in {1..3}; do
+        md5=$(md5sum $DB_PATH/d2.rodb | awk '{print $1}')
+        expected=$(cat $DB_PATH/d2.md5 | awk '{print $1}')
+        if [ "$md5" == "$expected" ]; then
+            echo "$DB_PATH/d2.rodb md5sum verified, attempt $i"
+            d2=true
+            break
+        else
+            echo "$DB_PATH/d2.rodb md5sum not verified, attempt $i"
+        fi
+   done
 fi
 
 docker-compose up -d --remove-orphans
