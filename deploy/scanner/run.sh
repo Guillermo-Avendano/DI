54002f49ddc4adacb198759cd83b5fa834331003
Guillermo-Avendano <avendano.guillermo@gmail.com>
2024-02-08 14:46:28 -0300

    va

--- deploy/scanner/run.sh
+++ deploy/scanner/run.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 docker-compose up -d --remove-orphans
-sleep 3
-docker exec mssql2017 /opt/mssql-tools/bin/sqlcmd -U sa -P "Rocket2023" -q "select count(*) from Northwind.dbo.customers go"
 
-if [ $? -ne 0 ]; then
-    docker exec mssql2017 /opt/mssql-tools/bin/sqlcmd -U sa -P "Rocket2023" -i /samples/pubs_db.sql
-    docker exec mssql2017 /opt/mssql-tools/bin/sqlcmd -U sa -P "Rocket2023" -i /samples/northwind_db.sql
-
-    sudo chmod -R 777 ./di_scanners_jobs
-    sudo chmod -R 777 ./di_scanners_workspace
-    sudo chmod -R 777 ./di_scanners_tomcat_logs
-
-fi
