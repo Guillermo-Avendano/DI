b96b0fed2c032cbc26a29d3bce983ecf2c28d3ee
Guillermo-Avendano <avendano.guillermo@gmail.com>
2024-02-14 14:27:09 -0300

    va

--- deploy/run.sh
+++ deploy/run.sh
@@ -3,7 +3,8 @@
 DI_SERVER_DATBAS=./di_server_datbas
 
 if [ -z "$(ls -A "$DI_SERVER_DATBAS")" ] ; then
-    echo "Rochade DB not inicialized!"
+    echo "Rochade DB is not inicialized!"
+    echo "run: ./init.sh"
 else
     docker-compose up -d --remove-orphans
 fi
