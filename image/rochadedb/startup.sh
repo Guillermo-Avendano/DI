258398ac380b6fcdd725b8bc67568d80d152451a
Guillermo-Avendano <avendano.guillermo@gmail.com>
2024-02-14 14:08:48 -0300

    va

--- image/rochadedb/startup.sh
+++ image/rochadedb/startup.sh
@@ -2,7 +2,4 @@
 
 tar -xzvf  /root/${DB}.tar.gz -C /home/rocket/rochade/datbas/ --strip-components=2
 
-# cp /root/${DB}.rodb /home/rocket/rochade/datbas/${DB}.rodb 
-# cp /root/${DB}.md5 /home/rocket/rochade/datbas/${DB}.md5
-
 chown -R 3003:0 /home/rocket/rochade/datbas
