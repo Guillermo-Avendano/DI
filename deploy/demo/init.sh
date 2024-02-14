89304f1f2f4a4611ff09c37a4fc474658cbebfba
Guillermo-Avendano <avendano.guillermo@gmail.com>
2024-02-14 14:15:41 -0300

    va

---
+++ deploy/demo/init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source .env
+
+docker-compose -f ./docker-compose.db.yaml up -d --remove-orphans
+
+DOTS="..."
+# Verificar si hay contenedores ejecutándose con la imagen especificada
+while docker ps --format '{{.Image}}' | grep -q $DI_DB1_IMAGE; do
+    echo "Waiting finish containers running with $DI_DB1_IMAGE image $DOTS"
+    sleep 2
+    DOTS="$DOTS..."
+done
+
+echo "$DI_DB1_IMAGE processed"
+
+# Verificar si hay contenedores ejecutándose con la imagen especificada
+while docker ps --format '{{.Image}}' | grep -q $DI_DB2_IMAGE; do
+    echo "Waiting finish containers running with $DI_DB2_IMAGE image $DOTS"
+    sleep 2
+    DOTS="$DOTS..."
+done
+
+echo "$DI_DB2_IMAGE processed"
+
